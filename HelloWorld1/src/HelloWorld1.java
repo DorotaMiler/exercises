dbfb0bf879043c4ec84cf65c86ebda88f94cbfc1
Dorota Miler <dorota.miler@outlook.com>
2019-12-28 18:41:24 +0100

    addition of another Hello World for IntelliJ process review on my PC

---
+++ HelloWorld1/src/HelloWorld1.java
@@ -0,0 +1,6 @@
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world again :)!");
+        System.out.println("Take care and all the best!");
+    }
+}
