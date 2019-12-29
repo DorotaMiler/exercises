f09bd88b3207b7ab31d4e7f54f1cc3c4d2cc4cb5
Dorota Miler <dorota.miler@outlook.com>
2019-12-29 18:31:00 +0100

    adjustemnt of comments

--- HelloWorld1/src/HelloWorld1.java
+++ HelloWorld1/src/HelloWorld1.java
@@ -1,11 +1,11 @@
 /**
  * this comment is used while creating program's documentation
 HeloWorld1 java calss addition
- @param args - parameters passed after running the table
+ @param args - parameters passed after running the program
 */
 
 public class HelloWorld1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {  // a starting method
         System.out.println("Hello world again :)!"); //Display the first String
         System.out.println("Take care and all the best!");  //Display the second String
     }
